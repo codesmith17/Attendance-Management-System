6aa2d8f55ed9431ffafb0431bdd43f23e5b91d05
codesmith17 <kanchanlatakrishna@gmail.com>
2024-01-12 22:22:31 +0530

    phph

---
+++ contact_us.php
@@ -0,0 +1,31 @@
+<?php
+include './Database/Controler.php';
+include 'role.php';
+?>
+
+<h2>Contact Details</h2>
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="table-responsive">
+
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <td>
+                            <center><br><img src="img/u.jpg" height="20%" width="30%" style="border-radius:50%;">
+                            </center><br>
+                            <b>
+                                <h3>
+                                    <center>KRISHNA TRIPATHI</center>
+                                </h3>
+                            </b>
+                            <h4>
+                                <center><b>Email:</b>kanchanlatakrishna@gmail.com</center>
+                                <h4>
+                                    <center><b>Contact Number:</b>123456789</center>
+                        </td>
+
+                    </table>
+                    <?php
+                    include 'footer.php';
+                    ?>
